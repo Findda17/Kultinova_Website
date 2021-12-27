60681e0d019217ac68f9f24b8c846b3be4bcbf7c
Findda17 <miguelsilvacastrillon@gmail.com>
2021-12-27 17:50:42 -0500

    Add files via upload

---
+++ public/assets/js/main.js
@@ -0,0 +1,57 @@
+/*=============== Navigation Box-shadow ===============*/
+$(window).on('scroll', function(){
+    if ($(window).scrollTop()){
+        $('nav').addClass('nav__scrolled');
+    }
+    else
+    {
+        $('nav').removeClass('nav__scrolled');
+    }
+})
+
+/*=============== SHOW CART ===============*/
+const cart = document.getElementById('cart'),
+      cartShop = document.getElementById('cart-shop'),
+      cartClose = document.getElementById('cart-close')
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+if(cartShop){
+    cartShop.addEventListener('click', () =>{
+        cart.classList.add('show-cart')
+    })
+}
+
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+if(cartClose){
+    cartClose.addEventListener('click', () =>{
+        cart.classList.remove('show-cart')
+    })
+}
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__img`, {delay: 200})
+sr.reveal(`.home__social`, {delay: 400})
+sr.reveal(`.steps__bg`)
+sr.reveal(`.steps__title`, {delay: 600})
+sr.reveal(`.about`)
+sr.reveal(`.about__data`, {delay: 600})
+sr.reveal(`.team`)
